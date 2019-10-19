4f84859373b9b6a9a6718a69bc9b697a24c84cfd
Michael Silver <masilver@gmail.com>
2019-10-19 00:43:12 -0400

    Fixed primary key issues

--- src/Identity.Dapper.Postgres/Models/RoleClaim.cs
+++ src/Identity.Dapper.Postgres/Models/RoleClaim.cs
@@ -4,6 +4,11 @@ namespace Identity.Dapper.Postgres.Models
 {
     internal class RoleClaim
     {
+        public RoleClaim()
+        {
+            Id = Guid.NewGuid();
+        }
+
         public Guid Id { get; set; }
         public Guid RoleId { get; set; }
         public string ClaimType { get; set; }
--- src/Identity.Dapper.Postgres/Models/UserClaim.cs
+++ src/Identity.Dapper.Postgres/Models/UserClaim.cs
@@ -4,6 +4,11 @@ namespace Identity.Dapper.Postgres.Models
 {
     internal class UserClaim
     {
+        public UserClaim()
+        {
+            Id = Guid.NewGuid();
+        }
+
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public string ClaimType { get; set; }
