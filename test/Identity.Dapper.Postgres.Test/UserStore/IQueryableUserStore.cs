d8aad2dd102784e6e3f37a441b8f7759b71683df
Michael Silver <masilver@gmail.com>
2019-10-22 11:04:46 -0400

    Fixed unit test.  Moved to MSTest

--- test/Identity.Dapper.Postgres.Test/UserStore/IQueryableUserStore.cs
+++ test/Identity.Dapper.Postgres.Test/UserStore/IQueryableUserStore.cs
@@ -1,11 +1,9 @@
 using System;
-using Xunit;
 
 namespace Identity.Dapper.Postgres.Test
 {
     public class IQueryableUserStore
     {
-        [Fact]
         public void Test1()
         {
 
--- test/Identity.Dapper.Postgres.Test/UserStore/UserStore.cs
+++ test/Identity.Dapper.Postgres.Test/UserStore/UserStore.cs
@@ -4,33 +4,34 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Identity.Dapper.Postgres.Test.UserStore
 {
+    [TestClass]
     public class UserStore
     {
-        [Fact]
+        [TestMethod]
         public void Constructor()
         {
             // assign
-            var usersTable = Mock.Of<UsersTable>();
-            var userRolesTable = Mock.Of<UserRolesTable>();
-            var rolesTable = Mock.Of<RolesTable>();
-            var userClaimsTable = Mock.Of<UserClaimsTable>();
-            var userLoginsTable = Mock.Of<UserLoginsTable>();
-            var userTokensTable = Mock.Of<UserTokensTable>();
+            var usersTable = new Mock<UsersTable>(null).Object;
+            var userRolesTable = new Mock<UserRolesTable>(null).Object;
+            var rolesTable = new Mock<RolesTable>(null).Object;
+            var userClaimsTable = new Mock<UserClaimsTable>(null).Object;
+            var userLoginsTable = new Mock<UserLoginsTable>(null).Object;
+            var userTokensTable = new Mock<UserTokensTable>(null).Object;
 
             // act
             var userStore = new S.UserStore(usersTable, userRolesTable, rolesTable, userClaimsTable, userLoginsTable, userTokensTable);
 
             // assert
-            Assert.Equal(usersTable, userStore._usersTable);
-            Assert.Equal(userRolesTable, userStore._userRolesTable);
-            Assert.Equal(rolesTable, userStore._rolesTable);
-            Assert.Equal(userClaimsTable, userStore._userClaimsTable);
-            Assert.Equal(userLoginsTable, userStore._userLoginsTable);
-            Assert.Equal(userTokensTable, userStore._userTokensTable);
+            Assert.AreEqual(usersTable, userStore._usersTable);
+            Assert.AreEqual(userRolesTable, userStore._userRolesTable);
+            Assert.AreEqual(rolesTable, userStore._rolesTable);
+            Assert.AreEqual(userClaimsTable, userStore._userClaimsTable);
+            Assert.AreEqual(userLoginsTable, userStore._userLoginsTable);
+            Assert.AreEqual(userTokensTable, userStore._userTokensTable);
         }
     }
 }
